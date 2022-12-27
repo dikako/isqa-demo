68a528a8f95c437d6d007bb49d61b14586695fde
fransiskus.setiawan <fransiskus.setiawan@stockbit.com>
2022-12-27 12:00:47 +0700

    dsds

--- src/main/java/demo/data/AutoOrder.java
+++ src/main/java/demo/data/AutoOrder.java
@@ -3,6 +3,7 @@ package demo.data;
 public class AutoOrder {
     private static String condition;
     private static String sscondition;
+    private static String ssscondition;
 
     public static void setCondition(String condition) {
         AutoOrder.condition = condition;
