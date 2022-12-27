00ec04b7cd20212ff34759b9aef6a0f5dcba89ea
fransiskus.setiawan <fransiskus.setiawan@stockbit.com>
2022-12-27 12:11:31 +0700

    fsa

--- src/main/java/demo/data/AutoOrder.java
+++ src/main/java/demo/data/AutoOrder.java
@@ -3,7 +3,6 @@ package demo.data;
 public class AutoOrder {
     private static String condition;
     private static String sscondition;
-    private static String ssscondition;
 
     public static void setCondition(String condition) {
         AutoOrder.condition = condition;
